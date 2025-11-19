770a6b06fd31a476bc226348ce0a4f0aff834160
Lynn <emmalynnchenn@gmail.com>
2025-11-19 17:16:51 -0600

    fixed an built error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,8 @@ export default function aboutPage() {
     <main className="bg-zinc-50">
       <div className="w-screen h-screen flex justify-center">
         <div className="w-2/3 mx-auto translate-y-8">
-          <p className="text-2xl text-black text-center">Hai I'm Lynn, I'm an ameture photographer, I enjoy taking photos of pretty things, and people. Lmk if you're interested to have your photos taken, you can reach me by my instagram :)</p>
+          <p className="text-2xl text-black text-center">Hai I&apos;m Lynn, I&apos;m an ameture photographer, I enjoy taking photos of pretty things and people. Lmk if you&apos;re interested to have your photos taken, you can reach me by my Instagram :)
+</p>
         </div>
       </div>
     </main >
